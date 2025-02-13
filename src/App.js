abe35b5b2f312bab126ab5d7c86d7278ee932dd3
Ziyed1 <zhammadi@sollan.com>
2025-02-13 18:18:54 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  // Appel à l'API backend (remplace l'URL par l'adresse réelle de ton backend)
+  useEffect(() => {
+    fetch('http://10.244.203.178:3000/api/message')  // L'URL de ton API
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <h1>Welcome to My Test App!</h1>
+        <p>
+          {data ? data.message : 'Loading...'}
+        </p>
+      </header>
     </div>
   );
 }
 
 export default App;
+
