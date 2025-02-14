1457824f2405bfd287e6067047d09d51450f48e3
Ziyed1 <zhammadi@sollan.com>
2025-02-14 16:33:08 +0100

    cors install

--- src/App.js
+++ src/App.js
@@ -2,37 +2,24 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const [data, setData] = useState(null);
-  const [error, setError] = useState(null); // Pour gérer les erreurs
 
   // Appel à l'API backend
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://10.110.23.98:31000/api/message');
-
-        // Vérification de la réponse
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const result = await response.json();
-        console.log('API Response:', result); // Affiche la réponse pour debugging
-        setData(result); // Mise à jour de l'état avec la réponse
-      } catch (err) {
-        console.error('Error fetching data:', err);
-        setError('Une erreur s\'est produite lors de la récupération des données.');
-      }
-    };
-
-    fetchData();
-  }, []); // Le tableau vide [] signifie que l'effet se déclenche uniquement au montage du composant
+    fetch('http://15.237.119.25:32653/api/message')  // Assure-toi d'utiliser l'URL de ton frontend ou du backend correct
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>IA Crypto - Découvre les tendances d'évolutions de Solana !</h1>
-        {error && <p style={{ color: 'red' }}>{error}</p>} {/* Affiche l'erreur si présente */}
-        <p>{data ? data.message : 'Chargement en cours...'}</p> {/* Affiche le message ou "Chargement..." */}
+        <p>
+          {data ? data.message : 'Loading... tkt ca arrive'}
+        </p>
       </header>
     </div>
   );
