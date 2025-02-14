6cc099567e52e97c830d65f8576477a5b35acf02
Ziyed1 <zhammadi@sollan.com>
2025-02-14 16:34:18 +0100

    cors server

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors'); // Import du package CORS
 const app = express();
 const port = 3000;
+
+// Active le CORS pour tous les domaines
+app.use(cors());
 
 app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
